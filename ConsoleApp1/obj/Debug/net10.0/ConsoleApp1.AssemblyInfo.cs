dabb406975c622ada95138d485893c87342ec60f
Ye Hein Thuta Aung <taki@192.168.1.19>
2026-07-18 15:16:11 +0630

    Add ConsoleApp1 and ConsoleApp2 folders

--- ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8caf07429b842c35ca3922b9cfe401fc1f1798de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+826169499bde09305fc1ccdebaff511c31708385")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
